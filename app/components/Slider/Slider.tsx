7d46fc2492acb374b28a8ae3ab0b1dbf4a28dbf5
ArtemPedenko <nubostrel@gmail.com>
2023-11-02 20:59:51 +0300

    doing slider

--- app/components/Slider/Slider.tsx
+++ app/components/Slider/Slider.tsx
@@ -7,7 +7,7 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/pagination';
 import './slider.css';
-import { Pagination } from 'swiper/modules';
+import { Pagination, Autoplay } from 'swiper/modules';
 
 const Slider = () => {
 	//console.log(data);
@@ -37,27 +37,35 @@ const Slider = () => {
 			spaceBetween='30px'
 			loop={true}
 			className='swiper-container-slider'
+			autoplay={{
+				delay: 100,
+				disableOnInteraction: false,
+			}}
 		>
 			<SwiperSlide className='swiper-slide-slider'>
 				<img
+					className='img'
 					alt='q'
 					src='https://ruslangrebnev.ru/wp-content/uploads/2022/11/1research.png'
 				/>
 			</SwiperSlide>
 			<SwiperSlide className='swiper-slide-slider'>
 				<img
+					className='img'
 					alt='q'
 					src='https://ruslangrebnev.ru/wp-content/uploads/2022/11/1research.png'
 				/>
 			</SwiperSlide>
 			<SwiperSlide className='swiper-slide-slider'>
 				<img
+					className='img'
 					alt='q'
 					src='https://ruslangrebnev.ru/wp-content/uploads/2022/11/1research.png'
 				/>
 			</SwiperSlide>
 			<SwiperSlide className='swiper-slide-slider'>
 				<img
+					className='img'
 					alt='q'
 					src='https://ruslangrebnev.ru/wp-content/uploads/2022/11/1research.png'
 				/>
