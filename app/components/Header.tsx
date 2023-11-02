7fa6b9fc75dae669a9892ae6d1880501bf10f3d8
Артем Педенко <131644157+ArtemPedenko@users.noreply.github.com>
2023-11-02 11:22:36 +0000

    Update Header.tsx and tailwind.config.js

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,9 +1,10 @@
 export default function Header() {
-    
-    return (
-        <header>
-            <div className='max-sm:bg-white max-xl:bg-black'>asd</div>
-        </header>
-    );
-
+  return (
+    <header>
+      <div className="border-2 border-red-600 flex flex-row justify-between h-[88px] max-w-[1175px]  w-full mx-auto px-[15px] pt-[32px] s:h-[108px]">
+        <img alt="logo" src="/logo.png" />
+        <div>2222</div>
+      </div>
+    </header>
+  );
 }
