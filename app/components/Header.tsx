6dedc58147a8f4741b68765bdfba977be7b3cb04
Артем Педенко <131644157+ArtemPedenko@users.noreply.github.com>
2023-11-02 13:26:33 +0000

    Update Header.tsx

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -3,10 +3,9 @@ import Image from "next/image";
 //
 export default function Header() {
   return (
-    <div className="border-2 border-red-600 flex flex-row justify-between items-end h-[88px] max-w-[1175px]  w-full mx-auto px-[15px] pt-[32px] s:h-[108px]">
+    <div className="border-2 border-red-700 flex flex-row justify-between items-center h-[120px] max-w-[1175px]  w-full mx-auto px-[15px] pt-[32px] s:h-[140px]">
       <img alt="logo" src="/logo.png" />
       <div>2222</div>
-      <div>3333</div>
     </div>
   );
 }
