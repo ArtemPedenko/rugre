96a7c24ce543f3aeaefc31da4235c9ac0b9b4f6c
ArtemPedenko <nubostrel@gmail.com>
2023-11-05 14:37:40 +0300

    header styling

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,19 +1,21 @@
-import MobileMenuLogo from "@/public/MobileMenuLogo";
-import Slider from "./Slider/Slider";
+import MobileMenuLogo from '@/public/MobileMenuLogo';
+import Slider from './Slider/Slider';
 
 //
 export default function Header() {
-  return (
-    <>
-      <div className="border-2 border-red-700 z-2 flex justify-between items-center h-30 max-w-[1175px]  w-full mx-auto px-4 pt-8 s:h-[140px]">
-        <img alt="logo" src="/logo.png" />
-        <div className="w-8 h-8">
-          <MobileMenuLogo />
-        </div>
-      </div>
-      <div className="w-full">
-        <Slider />
-      </div>
-    </>
-  );
+	return (
+		<div className='relative'>
+			<div className='relative z-20 flex justify-between items-center h-[104px] px-4 pt-4 max-w-[1175px] w-full mx-auto s:h-[140px] s:pt-8 md:pt-8'>
+				<img alt='logo' src='/logo.png' className='h-full' />
+				<div className='w-[68px] h-14'>
+					<MobileMenuLogo />
+				</div>
+			</div>
+			<div className='border-2 border-red-500 w-full mt-[-90px] z-1  '>
+				<Slider />
+			</div>
+		</div>
+	);
 }
+
+//<div className='w-full relative -top-[90px] z-10  '>
