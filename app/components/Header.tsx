b2b63b0549f5b9702a8f76d06845b2b2a34088ca
Артем Педенко <131644157+ArtemPedenko@users.noreply.github.com>
2023-11-09 13:20:44 +0000

    Update 3 files

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,23 +1,30 @@
-import MobileMenuLogo from '@/public/MobileMenuLogo';
-import Slider from './Header/Slider/Slider';
-import Button from './Header/Button';
+import MobileMenuLogo from "@/public/MobileMenuLogo";
+import Slider from "./Header/Slider/Slider";
+import Button from "./Header/Button";
 
 //
 export default function Header() {
-	return (
-		<div className='relative bg-light_green'>
-			<div className='relative z-20 flex justify-between items-center h-[104px] px-4 pt-4 max-w-[1175px] w-full mx-auto sm:h-[140px] sm:pt-8'>
-				<img alt='logo' src='/logo.png' className='h-full' />
-				<Button>НОВОСТИ</Button>
-				<div className='w-[68px] h-[68px] md:hidden '>
-					<MobileMenuLogo />
-				</div>
-			</div>
-			<div className='flex items-end w-full mt-[-90px] z-1 h-[265px] xxs:h-[317px] xs:h-[355px] sm:h-[400px] md:h-auto'>
-				<Slider />
-			</div>
-		</div>
-	);
+  return (
+    <div className="relative bg-light_green">
+      <div className="relative z-20 flex justify-between items-center h-[104px] px-4 pt-4 max-w-[1175px] w-full mx-auto sm:h-[140px] sm:pt-8">
+        <img alt="logo" src="/logo.png" className="h-full" />
+        <div className="hidden md:flex gap-4">
+          <Button>НОВОСТИ</Button>
+          <Button>БИБЛИОТЕКА</Button>
+          <Button>ВИДЕО</Button>
+          <Button>БИОГРАФИЯ</Button>
+          <Button>СОРАТНИКИ</Button>
+          <Button>КОНТАКТЫ</Button>
+        </div>
+        <div className="w-[68px] h-[68px] md:hidden ">
+          <MobileMenuLogo />
+        </div>
+      </div>
+      <div className="flex items-end w-full mt-[-90px] z-1 h-[265px] xxs:h-[317px] xs:h-[355px] sm:h-[400px] md:h-auto">
+        <Slider />
+      </div>
+    </div>
+  );
 }
 
 //<div className='w-full relative -top-[90px] z-10  '>
