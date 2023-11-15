c1b720b2d694dfde7c417fb7ab35122caf209cdd
Артем Педенко <131644157+ArtemPedenko@users.noreply.github.com>
2023-11-15 13:23:42 +0000

    Update 4 files

--- app/page.tsx
+++ app/page.tsx
@@ -2,16 +2,17 @@ import { getLatestPosts } from "@/lib/api";
 import Logger from "./Logger";
 
 export default async function Home() {
-	const data = await getLatestPosts();
-	return (
-		<main>
-			<Logger data={data.nodes[2].content} />
-			<div>HОВОСТИ</div>
-			{typeof data.nodes[0].content == "string" ? (
+  const data = await getLatestPosts();
+  console.log("asd");
+  return (
+    <main>
+      <Logger data={data} />
+      <div>HОВОСТИ</div>
+      {/* {typeof data.nodes[0].content == "string" ? (
 				<div dangerouslySetInnerHTML={{ __html: data.nodes[2].content }} />
-			) : null}
-		</main>
-	);
+			) : null} */}
+    </main>
+  );
 }
 
 export const revalidate = 0;
--- lib/api.ts
+++ lib/api.ts
@@ -1,39 +1,55 @@
 const API_URL: any = process.env.WP_URL;
 
 async function fetchAPI(query = "", { variables }: Record<string, any> = {}) {
-	const headers = { "Content-Type": "application/json" };
+  const headers = { "Content-Type": "application/json" };
 
-	const res = await fetch(API_URL, {
-		method: "POST",
-		headers,
-		body: JSON.stringify({ query, variables }),
-	});
+  const res = await fetch(API_URL, {
+    method: "POST",
+    headers,
+    body: JSON.stringify({ query, variables }),
+  });
 
-	const json = await res.json();
-	if (json.errors) {
-		console.log(json.errors);
-		throw new Error("Failed to fetch API");
-	}
+  const json = await res.json();
+  if (json.errors) {
+    console.log(json.errors);
+    throw new Error("Failed to fetch API");
+  }
 
-	return json.data;
+  return json.data;
 }
 
 export async function getLatestPosts() {
-	const data = await fetchAPI(
-		`
+  const data = await fetchAPI(
+    `
       query FavoitePosts {
-        posts(first: 10) {
-          nodes {
-            slug
-            title
+         posts {
+    nodes {
+      title
+      id
+      editorBlocks {
+        ... on CoreParagraph {
+          attributes {
             content
           }
         }
+        ... on CoreImage {
+          attributes {
+            src
+          }
+        }
+        ... on CoreFile {
+          attributes {
+            href
+          }
+        }
       }
-      `
-	);
+    }
+  }
+      }
+      `,
+  );
 
-	return data?.posts;
+  return data?.posts.nodes;
 }
 
 export const revalidate = 3;
