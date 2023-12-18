c558713fd376667d5d6f2ae65002419aa2ec3339
Artem Pedenko <131644157+ArtemPedenko@users.noreply.github.com>
2023-12-18 10:02:34 +0000

    auth testing

---
+++ app/admin/api/route.ts
@@ -0,0 +1,39 @@
+import { NextResponse } from "next/server";
+import { type NextRequest } from "next/server";
+import { cookies } from "next/headers";
+
+export async function POST(request: Request) {
+  const req = await request.json();
+  console.log(req);
+
+  /* const res = await fetch(req.url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, OPTIONS, PUT, PATCH, DELETE",
+      "Access-Control-Allow-Headers":
+        "Origin, X-Requested-With, Content-Type, Accept",
+    },
+    body: JSON.stringify({ login: req.login, password: req.password }),
+  });
+  const resJson = await res.json();
+  cookies().set("authorization", resJson.accessToken); */
+
+  const res = await fetch("https://arthttp.ru/api/user/refresh-token", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, OPTIONS, PUT, PATCH, DELETE",
+      "Access-Control-Allow-Headers":
+        "Origin, X-Requested-With, Content-Type, Accept",
+    },
+    body: JSON.stringify({ login: req.login }),
+  });
+  const resJson = await res.json();
+  console.log(resJson);
+  cookies().set("authorization", resJson.accessToken);
+
+  return new NextResponse(JSON.stringify(resJson));
+}
--- app/login/components/Form.tsx
+++ app/login/components/Form.tsx
@@ -3,76 +3,93 @@ import { Formik } from "formik";
 import * as Yup from "yup";
 
 export default function Form() {
+  async function test(login: string, password: string) {
+    fetch("https://arthttp.ru/api/user/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods":
+          "GET, POST, OPTIONS, PUT, PATCH, DELETE",
+        "Access-Control-Allow-Headers":
+          "Origin, X-Requested-With, Content-Type, Accept",
+      },
+      body: JSON.stringify({ login: login, password: password }),
+    })
+      .then((response) => response.json())
+      .then((data) => console.log(data))
+      .catch((error) => console.error(error));
+  }
+
   async function loginUser(login: string, password: string) {
-    console.log(login, password);
-    /* try {
-      const response = await fetch("http://45.147.176.226:4000/api/login", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ login: login, password: password }), // body data type must match "Content-Type" header
-      });
-      const res = await response;
-      console.log(res);
-    } catch (e) {
-      console.log(e);
-    } */
-    /* const response = await fetch(
-      "https://humble-cod-4xvwx6x4vv53q9pq-4000.app.github.dev/api/posts/post"
-    );
-    console.log(response); */
+    fetch("/admin/api", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        login: login,
+        password: password,
+        url: "https://arthttp.ru/api/user/login",
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => console.log(data))
+      .catch((error) => console.error(error));
   }
 
   return (
-    <Formik
-      initialValues={{ login: "", password: "" }}
-      onSubmit={(values) => loginUser(values.login, values.password)}
-      validateOnChange={false}
-      validateOnBlur={false}
-      validationSchema={Yup.object().shape({
-        login: Yup.string().min(3).required("Обязательное поле"),
-        password: Yup.string().min(3).required("Обязательное поле"),
-      })}
-    >
-      {(props) => {
-        props.submitCount > 0 && (props.validateOnChange = true);
-        const { values, errors, handleChange, handleSubmit } = props;
-        return (
-          <form
-            onSubmit={handleSubmit}
-            className="flex flex-col justify-center items-center gap-1"
-          >
-            <input
-              id="login"
-              placeholder={`enter login`}
-              type="text"
-              value={values.login}
-              onChange={handleChange}
-              className="border-2 border-black rounded-md"
-            />
-            {errors.login ? (
-              <div style={{ color: "red" }}>{errors.login}</div>
-            ) : null}
+    <>
+      <Formik
+        initialValues={{ login: "", password: "" }}
+        onSubmit={(values) => loginUser(values.login, values.password)}
+        validateOnChange={false}
+        validateOnBlur={false}
+        validationSchema={Yup.object().shape({
+          login: Yup.string().min(3).required("Обязательное поле"),
+          password: Yup.string().min(3).required("Обязательное поле"),
+        })}
+      >
+        {(props) => {
+          props.submitCount > 0 && (props.validateOnChange = true);
+          const { values, errors, handleChange, handleSubmit } = props;
+          return (
+            <form
+              onSubmit={handleSubmit}
+              className="flex flex-col justify-center items-center gap-1"
+            >
+              <input
+                id="login"
+                placeholder={`enter login`}
+                type="text"
+                value={values.login}
+                onChange={handleChange}
+                className="border-2 border-black rounded-md"
+              />
+              {errors.login ? (
+                <div style={{ color: "red" }}>{errors.login}</div>
+              ) : null}
 
-            <input
-              id="password"
-              placeholder={`enter password`}
-              type="password"
-              value={values.password}
-              onChange={handleChange}
-              className="border-2 border-black rounded-md"
-            />
-            {errors.password ? (
-              <div style={{ color: "red" }}>{errors.password}</div>
-            ) : null}
+              <input
+                id="password"
+                placeholder={`enter password`}
+                type="password"
+                value={values.password}
+                onChange={handleChange}
+                className="border-2 border-black rounded-md"
+              />
+              {errors.password ? (
+                <div style={{ color: "red" }}>{errors.password}</div>
+              ) : null}
 
-            <button type="submit" onClick={() => null}>
-              sign in
-            </button>
-          </form>
-        );
-      }}
-    </Formik>
+              <button type="submit" onClick={() => null}>
+                sign in
+              </button>
+            </form>
+          );
+        }}
+      </Formik>
+      <button onClick={() => test("root", "Hlsdf34=")}>test</button>
+    </>
   );
 }
