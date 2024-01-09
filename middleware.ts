1c8dfed949aabc8460b02f229adbd1db74fa5804
Artem Pedenko <131644157+ArtemPedenko@users.noreply.github.com>
2024-01-09 09:13:59 +0000

    bug fix with wrong refreshtoken

--- middleware.ts
+++ middleware.ts
@@ -4,33 +4,39 @@ import jwt from "jsonwebtoken";
 import { cookies } from "next/headers";
 import { refreshAccessToken } from "@/app/utils/services/userService";
 import { isTokenValid } from "@/app/utils/services/jwtService";
+import { createUnparsedSourceFile } from "typescript";
 
 export const config = {
   matcher: "/admin/((?!api|_next/static|_next/image|favicon.ico).*)",
 };
 
 export async function middleware(request: NextRequest, response: NextResponse) {
-  const cookieStore = cookies();
-  const accessToken = cookieStore.get("accessToken")?.value;
-  const refreshToken = cookieStore.get("refreshToken")?.value;
+  try {
+    const cookieStore = cookies();
+    const accessToken = cookieStore.get("accessToken")?.value;
+    const refreshToken = cookieStore.get("refreshToken")?.value;
 
-  if (!refreshToken) {
+    if (!refreshToken) {
+      return NextResponse.rewrite(new URL("/login", request.url));
+    }
+
+    const isRefreshTokenValid = isTokenValid(refreshToken, "root");
+
+    if (!isRefreshTokenValid || isRefreshTokenValid.exp < 0) {
+      return NextResponse.rewrite(new URL("/login", request.url));
+    }
+    if (!accessToken) {
+      return await fRefreshAccessToken(isRefreshTokenValid.login, refreshToken);
+    }
+
+    const isAccessTokenValid = isTokenValid(accessToken, "root");
+
+    if (!isAccessTokenValid || isAccessTokenValid.exp < 60000) {
+      return await fRefreshAccessToken(isRefreshTokenValid.login, refreshToken);
+    }
+  } catch (e) {
+    console.log(e);
     return NextResponse.rewrite(new URL("/login", request.url));
-  }
-
-  const isRefreshTokenValid = isTokenValid(refreshToken, "root");
-
-  if (!isRefreshTokenValid || isRefreshTokenValid.exp < 0) {
-    return NextResponse.rewrite(new URL("/login", request.url));
-  }
-  if (!accessToken) {
-    return await fRefreshAccessToken(isRefreshTokenValid.login, refreshToken);
-  }
-
-  const isAccessTokenValid = isTokenValid(accessToken, "root");
-
-  if (!isAccessTokenValid || isAccessTokenValid.exp < 60000) {
-    return await fRefreshAccessToken(isRefreshTokenValid.login, refreshToken);
   }
 }
 
